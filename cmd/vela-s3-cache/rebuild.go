2063c45000759f8e8be0da5015c48001bf158969
wass3r <1301201+wass3r@users.noreply.github.com>
2025-04-07 15:20:14 -0500

    use new archiver

--- cmd/vela-s3-cache/rebuild.go
+++ cmd/vela-s3-cache/rebuild.go
@@ -3,6 +3,7 @@
 package main
 
 import (
+	"compress/flate"
 	"context"
 	"fmt"
 	"os"
@@ -10,9 +11,10 @@ import (
 	"time"
 
 	"github.com/dustin/go-humanize"
-	"github.com/go-vela/archiver/v3"
 	"github.com/minio/minio-go/v7"
 	"github.com/sirupsen/logrus"
+
+	"github.com/go-vela/vela-s3-cache/pkg/archiver"
 )
 
 const rebuildAction = "rebuild"
@@ -21,6 +23,8 @@ const rebuildAction = "rebuild"
 type Rebuild struct {
 	// sets the name of the bucket
 	Bucket string
+	// set the compression level for the archive
+	CompressionLevel int
 	// sets the path for where to store the object
 	Path string
 	// sets the prefix for where to store the object
@@ -41,56 +45,61 @@ type Rebuild struct {
 func (r *Rebuild) Exec(mc *minio.Client) error {
 	logrus.Trace("running rebuild with provided configuration")
 
-	t := archiver.NewTarGz()
-	t.PreservePath = r.PreservePath
+	p := filepath.Join(os.TempDir(), r.Filename)
 
-	logrus.Debug("determining temp directory for archive")
+	logrus.Debugf("determined temporary file path as %s", p)
 
-	f := filepath.Join(os.TempDir(), r.Filename)
+	f, err := os.Create(p)
+	if err != nil {
+		return fmt.Errorf("failed to create temporary file %s for cache archive: %w", p, err)
+	}
+	defer os.Remove(f.Name())
 
-	logrus.Debugf("archiving artifact in path %s", f)
+	logrus.Debugf("created temporary file %s", f.Name())
 
-	// archive the objects in the mount path provided
-	err := t.Archive(r.Mount, f)
+	// forcing format until we support more formats
+	a, err := archiver.NewArchiver("tar.gz",
+		archiver.WithCompressionLevel(r.CompressionLevel),
+		archiver.WithPreservePath(r.PreservePath),
+	)
 	if err != nil {
 		return err
 	}
 
-	stat, err := os.Stat(f)
+	// archive the objects in the mount paths provided
+	err = a.Archive(context.Background(), r.Mount, f)
 	if err != nil {
 		return err
 	}
 
-	logrus.Infof("archive %s created, %s", f, humanize.Bytes(uint64(stat.Size())))
+	logrus.Debugf("archiving artifact in path %s complete", f.Name())
 
-	logrus.Debugf("opening artifact %s for reading", f)
-
-	obj, err := os.Open(f)
+	stat, err := os.Stat(f.Name())
 	if err != nil {
 		return err
 	}
 
-	logrus.Debugf("archive %s opened for reading", f)
+	//nolint:gosec // G115: integer overflow conversion should be handled via max()
+	logrus.Infof("archive %s created with size %s", f.Name(), humanize.Bytes(uint64(max(0, stat.Size()))))
 
 	// set a timeout on the request to the cache provider
 	ctx, cancel := context.WithTimeout(context.Background(), r.Timeout)
 	defer cancel()
 
-	logrus.Debugf("putting archive %s in bucket %s in path: %s", f, r.Bucket, r.Namespace)
+	logrus.Debugf("putting archive %s in bucket %s in path: %s", f.Name(), r.Bucket, r.Namespace)
 
 	// create an options object for the upload
 	mObj := minio.PutObjectOptions{
-		ContentType: "application/tar",
+		ContentType: "application/gzip", // gzip is the closest for tar.gz https://www.iana.org/assignments/media-types/media-types.xhtml
 	}
 
-	// upload the object to the specified location in the bucket
-	n, err := mc.PutObject(ctx, r.Bucket, r.Namespace, obj, -1, mObj)
+	n, err := mc.FPutObject(ctx, r.Bucket, r.Namespace, f.Name(), mObj)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to upload cache archive to bucket %s at path %s: %w", r.Bucket, r.Namespace, err)
 	}
 
-	u := uint64(n.Size)
-	logrus.Infof("cache rebuild action completed. %s of data rebuilt and stored", humanize.Bytes(u))
+	//nolint:gosec // G115: integer overflow conversion should be handled via max()
+	logrus.Infof("cache rebuild action completed. %s of data rebuilt and stored", humanize.Bytes(uint64(max(0, n.Size))))
 
 	return nil
 }
@@ -138,8 +147,14 @@ func (r *Rebuild) Validate() error {
 	for _, mount := range r.Mount {
 		_, err := os.Lstat(mount)
 		if err != nil {
-			return fmt.Errorf("mount: %s, make sure file or directory exists", mount)
+			return fmt.Errorf("mount not found: %s, make sure file or directory exists", mount)
 		}
+	}
+
+	// verify valid compression level is provided
+	// using compress/flate levels as a guide for compression level range
+	if r.CompressionLevel < flate.DefaultCompression || r.CompressionLevel > flate.BestCompression {
+		return fmt.Errorf("compression level must be between -1 and 9")
 	}
 
 	return nil
