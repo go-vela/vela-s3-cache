0d21dc456318ebaf584b6bb72bfbb8ab4d3ba781
wass3r <1301201+wass3r@users.noreply.github.com>
2025-04-09 15:14:23 -0500

    fix spelling error

--- version/version.go
+++ version/version.go
@@ -28,7 +28,7 @@ var (
 	Tag = "v0.0.0"
 )
 
-// New creates a new version object for Vela that is used throughout the aWpplication.
+// New creates a new version object for Vela that is used throughout the application.
 func New() *version.Version {
 	v, err := semver.NewVersion(Tag)
 	if err != nil {
